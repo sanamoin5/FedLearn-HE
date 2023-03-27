acacb749f8605d66254ce96acb0c2bc1014da813
Sana Moin <sana.moin.star@gmail.com>
2023-03-27 18:38:47 +0200

    Updated arguments

--- models/args_parser.py
+++ models/args_parser.py
@@ -7,12 +7,16 @@ def args_parser():
     # federated arguments (Notation for the arguments followed from paper)
     parser.add_argument('--fed_algo', type=str, default='FedAvg',
                         help="Specify federated aggregation algorithm to use(FedAvg, FedProx)")
+    parser.add_argument('--aggregation_type', type=str, default='weights',
+                        help="Set to 'weights'/'gradients' to specify federated aggregation type, default is weights")
     parser.add_argument('--rounds', type=int, default=10,
-                        help="number of rounds of training")
-    parser.add_argument('--num_users', type=int, default=2,
-                        help="number of users: K")
+                        help="number of rounds of training ()Default: 10")
+    parser.add_argument('--num_clients', type=int, default=2,
+                        help="number of clients (Default: 2)")
+    parser.add_argument('--client_weights', nargs='+', type=float, default=None,
+                        help="weights for each of the client")
     parser.add_argument('--epochs', type=int, default=10,
-                        help="the number of local epochs: E")
+                        help="the number of local epochs (Default: 10)")
     parser.add_argument('--local_bs', type=int, default=64,
                         help="local batch size: B")
     parser.add_argument('--lr', type=float, default=0.001,
@@ -31,20 +35,30 @@ def args_parser():
                              'provided by the context if it is set to None.')
     parser.add_argument('--poly_modulus_degree', type=int, default=8192,
                         help='The degree of the polynomial modulus, must be a power of two')
-    parser.add_argument('--coeff_mod_bit_sizes', type=list, default=[40, 20, 40],
+    parser.add_argument('--coeff_mod_bit_sizes', nargs='+', type=int, default=[40, 20, 40],
                         help='List of bit size for each coefficient modulus. Can be an empty list for BFV, a default '
                              'value will be given.')
     parser.add_argument('--global_scale', type=int, default=40,
                         help='the scaling factor')
-    parser.add_argument('--create_galois_keys', type=bool, default=False,
-                        help='Enables generation of public keys needed to perform encrypted vector rotation operations '
-                             'on batched ciphertexts.')
+
+    parser.add_argument('--create_galois_keys', dest='create_galois_keys', action='store_true',
+                        help="Enables generation of public keys needed to perform encrypted vector rotation operations"
+                             " on batched ciphertexts (default not set)")
+    parser.add_argument('--no_galois_keys', dest='create_galois_keys', action='store_false',
+                        help="Disables generation of public keys needed to perform encrypted vector rotation operations"
+                             " on batched ciphertexts (default set)")
+    parser.set_defaults(create_galois_keys=False)
+
     parser.add_argument('--plain_modulus', type=int, default=1032193,
                         help='The plaintext modulus. Should not be passed when the scheme is CKKS.')
 
     # other arguments
-    parser.add_argument('--test_without_encryption', type=bool, default=True,
-                        help="Set to False if model does not want to be tested without encryption, default True.")
+    parser.add_argument('--test_with_encryption', dest='test_without_encryption', action='store_true',
+                        help="Set if model wants to be tested with encryption, default set")
+    parser.add_argument('--test_without_encryption', dest='test_without_encryption', action='store_false',
+                        help="Set if model does not want to be tested with encryption (default not set).")
+    parser.set_defaults(test_without_encryption=True)
+
     parser.add_argument('--dataset', type=str, default='mnist', help="name \
                         of dataset(mnist, cifar-10)")
     parser.add_argument('--num_classes', type=int, default=10, help="number \
